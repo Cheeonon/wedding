0e31af30d8573dc5d76aee6f448d52959bae2cc6
cheeonon <cheeon00@gmail.com>
2023-09-10 00:36:53 -0400

    .

--- src/components/Web/Web.js
+++ src/components/Web/Web.js
@@ -26,26 +26,26 @@ const Web = () => {
             <br></br>
             <div className="block block-page">
                 <div className="block__paragraph">
-                    <p className="block__line">한성희 정미옥의 장남 <b>한규희</b></p>
-                    <p className="block__line">박성우 김성희의 장녀 <b>박채린</b></p>
+                    <p className="block__line korean">한성희 정미옥의 장남 <b>한규희</b></p>
+                    <p className="block__line korean">박성우 김성희의 장녀 <b>박채린</b></p>
                 </div>
                 <div className="block__paragraph">
-                    <p className="block__line">저희 두 사람, 이제는 한 몸을 이루어</p>
-                    <p className="block__line">오직 여호와를 섬기며 함께 열매 맺겠노라<br></br> 서약하려 합니다.</p>
-                    <p className="block__line">이 기쁜 순간을 함께하고 싶기에,<br></br> 귀하를 초대합니다.</p>
+                    <p className="block__line korean">저희 두 사람, 이제는 한 몸을 이루어</p>
+                    <p className="block__line korean">오직 여호와를 섬기며 함께 열매 맺겠노라<br></br> 서약하려 합니다.</p>
+                    <p className="block__line korean">이 기쁜 순간을 함께하고 싶기에,<br></br> 귀하를 초대합니다.</p>
                 </div>
             </div>
             <div className="horizontalLine"></div>
             <div className="block block-page">
-                <div className="block__paragraph block--english">
-                    <p className="block__line">ceremony | 11 : 00 AM</p>
-                    <p className="block__line">"LAWRENCE PARK COMMUNITY CHURCH"</p>
-                    <p className="block__line">2180 BAYVIEW AVE, NORTH YORK</p>
+                <div className="block__paragraph block">
+                    <p className="block__line english">ceremony | 11 : 00 AM</p>
+                    <p className="block__line english">"LAWRENCE PARK COMMUNITY CHURCH"</p>
+                    <p className="block__line english">2180 BAYVIEW AVE, NORTH YORK</p>
                 </div>
-                <div className="block__paragraph block--english">
-                    <p className="block__line">reception | 12 : 30 PM</p>
-                    <p className="block__line">"AUBERGE DU POMMIER"</p>
-                    <p className="block__line">4150 YONGE ST, NORTH YORK</p>
+                <div className="block__paragraph block">
+                    <p className="block__line english">reception | 12 : 30 PM</p>
+                    <p className="block__line english">"AUBERGE DU POMMIER"</p>
+                    <p className="block__line english">4150 YONGE ST, NORTH YORK</p>
                 </div>
             </div>
             <div className="block block-page">
