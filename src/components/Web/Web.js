5e2d8b4c03d154078d623c860810e9cb6d9972fe
cheeonon <cheeon00@gmail.com>
2023-09-10 01:29:06 -0400

    .

--- src/components/Web/Web.js
+++ src/components/Web/Web.js
@@ -19,38 +19,37 @@ const Web = () => {
     console.log(data)
 
     return (
-        <div className="web">
+        <main className="web">
             <div className="block">
                 <img className="block__img" src={cover} alt="대문" />
             </div>
-            <br></br>
-            <div className="block block-page">
+            <div className="block">
                 <div className="block__paragraph">
-                    <p className="block__line korean">한성희 정미옥의 장남 <b>한규희</b></p>
-                    <p className="block__line korean">박성우 김성희의 장녀 <b>박채린</b></p>
+                    <p className="korean">한성희 정미옥의 장남 <b>한규희</b></p>
+                    <p className="korean">박성우 김성희의 장녀 <b>박채린</b></p>
                 </div>
                 <div className="block__paragraph">
-                    <p className="block__line korean">저희 두 사람, 이제는 한 몸을 이루어</p>
-                    <p className="block__line korean">오직 여호와를 섬기며 함께 열매 맺겠노라<br></br> 서약하려 합니다.</p>
-                    <p className="block__line korean">이 기쁜 순간을 함께하고 싶기에,<br></br> 귀하를 초대합니다.</p>
+                    <p className="korean">저희 두 사람, 이제는 한 몸을 이루어</p>
+                    <p className="korean">오직 여호와를 섬기며 함께 열매 맺겠노라<br></br> 서약하려 합니다.</p>
+                    <p className="korean">이 기쁜 순간을 함께하고 싶기에,<br></br> 귀하를 초대합니다.</p>
                 </div>
             </div>
             <div className="horizontalLine"></div>
-            <div className="block block-page">
+            <div className="block">
                 <div className="block__paragraph block">
-                    <p className="block__line english">ceremony | 11 : 00 AM</p>
-                    <p className="block__line english">"LAWRENCE PARK COMMUNITY CHURCH"</p>
-                    <p className="block__line english">2180 BAYVIEW AVE, NORTH YORK</p>
+                    <p>ceremony | 11 : 00 AM</p>
+                    <p>"LAWRENCE PARK COMMUNITY CHURCH"</p>
+                    <p>2180 BAYVIEW AVE, NORTH YORK</p>
                 </div>
                 <div className="block__paragraph block">
-                    <p className="block__line english">reception | 12 : 30 PM</p>
-                    <p className="block__line english">"AUBERGE DU POMMIER"</p>
-                    <p className="block__line english">4150 YONGE ST, NORTH YORK</p>
+                    <p>reception | 12 : 30 PM</p>
+                    <p>"AUBERGE DU POMMIER"</p>
+                    <p>4150 YONGE ST, NORTH YORK</p>
                 </div>
             </div>
-            <div className="block block-page">
+            <div className="block">
                 <form className="block__form" onSubmit={handleSubmit}>
-                    <h2 className="block__title">RSVP</h2> <span className='block__title--small'>by <b>20TH OF OCTOBER</b></span>
+                    <h1 className="block__title">RSVP</h1> <span className='block__title--small'>by <b>20TH OF OCTOBER</b></span>
 
                     <div className="form__block">
                         <label className="form__label" htmlFor='name'>NAME</label>
@@ -87,7 +86,7 @@ const Web = () => {
                     <input className="form__submit" type="submit" value="Submit"/>
                 </form>
             </div>
-        </div>
+        </main>
     )
 }
 
